d053e498b9e26df977409d0699d58e332d130943
Ntando <mngnta011@myuct.ac.za>
2024-08-24 11:07:25 +0200

    Final video

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../styles/VideoSection.css';
-import video from '../assets/okay.mp4'
+import video from '../assets/my-g-bd-vd.mp4'
 
 const VideoSection = () => {
   return (
     <div className="video-container">
-      <h1>Happy Birthday</h1>
+      <h1>Happy 20th Birthday</h1>
       <video className="video" width="750" height="500" controls autoPlay loop>
       <source src={video} type="video/mp4" />
         Your browser does not support the video tag.
